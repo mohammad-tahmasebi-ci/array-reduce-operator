f262f2916bc4be53300ab76fdc2666d39088e67a
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-04 23:08:59 +0000

    answer to reduce challenge

--- reduce.js
+++ reduce.js
@@ -4,9 +4,30 @@
  * command node reduce.js in the terminal
  */
 
-
+/*
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum1 = nums.reduce((acc, curr) => {
+console.log(
+  'Accumulator:', acc,
+  'Current value: ', curr,
+  'Total: ', acc + curr);
+  return acc + curr;
+});  // no initial accumulator value has been provided
+console.log(sum1);
 
+let sum2 = nums.reduce((acc, curr) => {
+  console.log(
+    'Accumulator: ', acc,
+    'Current value: ', curr,
+    'Total:', acc + curr
+  );
+  return acc + curr;
+}, 0);  // 0 here is the initial accumulator value
+console.log(sum2);
+
+let sum3 = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum3);
 
 const teamMembers = [
   {
@@ -32,6 +53,58 @@ const teamMembers = [
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});  // {} denotes the initial object value to be used for the accumulator
+
+console.log(experienceByProfession);
+*/
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+const biggest = students.reduce((acc, curr) => {
+  let aname = '';
+  let max = Math.max(acc, curr.results.english);
+  if (curr.results.english === max) { 
+    aname = curr.name; 
+  }
+  console.log(aname, max);
+  return {...acc, [curr.name]: aname, max: max};
+}, {});
+
+console.log(biggest);
